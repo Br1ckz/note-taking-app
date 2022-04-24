7370cb509838bfe946f6592f80039c870d777351
MinimumPort47 <MinimumPort47@Miller-PC>
2022-04-24 11:10:26 -0400

    Added Note Controller. Added showNoteUI method. Added returnToLogin method.

---
+++ src/mvc/controller/NoteCntl.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package mvc.controller;
+
+import mvc.model.DBConnection;
+import mvc.view.NoteView;
+
+/**
+ *
+ * @author Joseph Miller
+ *
+ */
+
+/*
+**************************** MODIFICATION LOG ****************************
+*/
+public class NoteCntl{
+    private final DBConnection dbConnection;
+    private final NoteView noteview;
+    private final LoginCntl loginview;
+    
+    public NoteCntl(){
+        noteview = new NoteView();
+        showNoteUI(true);
+        
+        dbConnection = new DBConnection();
+        
+        loginview = new LoginCntl();
+        this.loginview.showLoginUI(false);
+    }
+    
+//    public void sendNote (String note) {
+//        this.dbConnection.setConnection(connection);
+//    }
+    
+    public void showNoteUI(Boolean bool) { 
+        noteview.setVisible(bool);
+        if (bool == false) {
+                noteview.dispose();
+        }
+    }
+    
+    public void returnToLogin() {
+        loginview.showLoginUI(true);
+        showNoteUI(false);
+    }
+
+}// end of class NoteCntl
