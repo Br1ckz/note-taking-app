d0e15049f7ed43cb5377af2646e44ff3ba48e733
Bryce Ciemiewicz <bciemiewicz@tutanota.com>
2022-04-25 16:03:18 -0400

    add MVC functionality

--- src/mvc/controller/NoteCntl.java
+++ src/mvc/controller/NoteCntl.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package mvc.controller;
 
 import mvc.model.DBConnection;
@@ -16,37 +15,36 @@ import mvc.view.NoteView;
 
 /*
 **************************** MODIFICATION LOG ****************************
-*/
-public class NoteCntl{
-    private final DBConnection dbConnection;
-    private final NoteView noteview;
-    private final LoginCntl loginview;
-    
-    public NoteCntl(){
-        noteview = new NoteView();
-        showNoteUI(true);
-        
-        dbConnection = new DBConnection();
-        
-        loginview = new LoginCntl();
-        this.loginview.showLoginUI(false);
-    }
-    
-    public void sendNote (int userId, int noteID, String noteBody) {
-        this.dbConnection.insertNote(noteID, noteBody);
-        this.dbConnection.insertNoteUser(userId, noteID);
-    }
-    
-    public void showNoteUI(Boolean bool) { 
-        noteview.setVisible(bool);
-        if (bool == false) {
-                noteview.dispose();
-        }
-    }
-    
-    public void returnToLogin() {
-        loginview.showLoginUI(true);
-        showNoteUI(false);
-    }
+ */
+public class NoteCntl {
+
+	private final DBConnection dbConnection;
+	private final NoteView noteview;
+	private final NavCntl navCntl;
+
+	public NoteCntl(NavCntl navCntl, DBConnection dbConnection) {
+		noteview = new NoteView(this);
+		showNoteUI(true);
+
+		this.dbConnection = new DBConnection();
+		this.navCntl = navCntl;
+	}
+
+	public void sendNote(int userId, int noteID, String noteBody) {
+		this.dbConnection.insertNote(noteID, noteBody);
+		this.dbConnection.insertNoteUser(userId, noteID);
+	}
+
+	public void showNoteUI(Boolean bool) {
+		noteview.setVisible(bool);
+		if (bool == false) {
+			noteview.dispose();
+		}
+	}
+
+	public void giveNavControl() {
+		navCntl.showNavUI(true);
+		showNoteUI(false);
+	}
 
 }// end of class NoteCntl
